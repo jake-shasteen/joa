847e2b6eaf67f3bcc67754632e53dce3cd9cea3a
Jake Shasteen <jakeshasteen@gmail.com>
2017-07-15 00:27:41 -0700

    Add button to collapse table of contents

--- public/index.js
+++ public/index.js
@@ -1,6 +1,30 @@
-$('.table-of-contents').append(
-  $('<h2 class="table-of-contents__header">Table of Contents</h2>'),
-  $('<ul class="table-of-contents__list">')
-    .append($('<li><a href="/">Home</a></li>'))
-    .append($('<li><a href="/seaofbrass">Sea of Brass</a></li>'))
-);
+(function() {
+
+  var tableOfContentsOpen = true;
+
+  function openTableOfContents() {
+    $('.table-of-contents').append(
+    $('<h2 class="table-of-contents__header">Table of Contents</h2>'),
+    $('<ul class="table-of-contents__list">')
+      .append($('<li><a href="/">Home</a></li>'))
+      .append($('<li><a href="/seaofbrass">Sea of Brass</a></li>'))
+    );
+  };
+
+  function closeTableOfContents() {
+    $('.table-of-contents').empty();
+  }
+
+  openTableOfContents();
+
+  $('.table-of-contents__toggle').click(function(){
+    if (tableOfContentsOpen) {
+      tableOfContentsOpen = false;
+      closeTableOfContents();
+    } else {
+      tableOfContentsOpen = true;
+      openTableOfContents();
+    }
+  });
+  
+})();
