57a0e9d0e2b3312791cfedd43aba5921af383df2
Jake Shasteen <jakeshasteen@gmail.com>
2017-07-15 00:08:43 -0700

    Make it look liked curled paper

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 $('.table-of-contents').append(
   $('<h2 class="table-of-contents__header">Table of Contents</h2>'),
   $('<ul class="table-of-contents__list">')
-    .append($('<li><a href="/">Jet Owners Anonymous Home</a></li>'))
+    .append($('<li><a href="/">Home</a></li>'))
     .append($('<li><a href="/seaofbrass">Sea of Brass</a></li>'))
 );
