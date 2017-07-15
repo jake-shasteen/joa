109e1c680d9088683a65ef3b4527dfdebc3320a7
Jake Shasteen <jakeshasteen@gmail.com>
2017-07-14 22:50:10 -0700

    Without index.html please

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 $('.table-of-contents').append(
   $('<h2 class="table-of-contents__header">Table of Contents</h2>'),
   $('<ul class="table-of-contents__list">')
-    .append($('<li><a href="/seaofbrass/index.html">Sea of Brass</a></li>'))
+    .append($('<li><a href="/seaofbrass">Sea of Brass</a></li>'))
 );
